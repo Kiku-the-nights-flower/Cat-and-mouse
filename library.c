a442611b7dc6e728aea6df9f19acedd064bb66c5
Mikulas Kopecky <mikulas@ka.cz>
2026-01-02 15:03:30 +0100

    Initial library commit

---
+++ library.c
@@ -0,0 +1,33 @@
+﻿#include "library.h"
+#include <stdio.h>
+#include <windows.h>
+
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT
+#endif
+
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    switch (fdwReason) {
+        case DLL_PROCESS_ATTACH:
+            MessageBoxA(nullptr,
+                        "C Library Injection Successful!",
+                        "CVE-2025-59489 Test",
+                        MB_OK | MB_ICONEXCLAMATION);
+            break;
+
+        case DLL_PROCESS_DETACH:
+            break;
+        default:
+            return FALSE;
+    }
+    return TRUE;
+}
+
+
+
+
+
+
---
+++ library.h
@@ -0,0 +1,6 @@
+﻿#ifndef CAT_AND_MOUSE_LIBRARY_H
+#define CAT_AND_MOUSE_LIBRARY_H
+
+#include <windows.h>
+
+#endif // CAT_AND_MOUSE_LIBRARY_H
